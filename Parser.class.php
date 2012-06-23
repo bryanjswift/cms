14ac0582c59ff58ebace72a7c18448f16219f7a7
Bryan J Swift <bryan.j.swift@gmail.com>
2012-06-23 14:59:02 -0400

    Change the expected value of prefix
    Make it only an XML prefix style that way a Tag's type can be determined
    by what comes after the colon.

--- Parser.class.php
+++ Parser.class.php
@@ -5,10 +5,10 @@ require_once("Tag.class.php");
 class Parser {
 
   private $template = "";
-  private $prefix = "cms:content";
+  private $prefix = "cms:";
   private $tags = array();
 
-  function __construct($template, $prefix="cms:content") {
+  function __construct($template, $prefix="cms:") {
     $fh = fopen($template, "r");
     $this->template = fread($fh, filesize($template));
     fclose($fh);
@@ -18,7 +18,7 @@ class Parser {
 
   function tags() {
     $matches = array();
-    $needle = "/<$this->prefix [^>]*>/";
+    $needle = "/<\/?" . $this->prefix . "([a-zA-Z0-9]*)( [^>]*)?>/";
     preg_match_all($needle, $this->template, $matches, PREG_SET_ORDER);
     foreach ($matches as $match) {
       array_push($this->tags, new Tag($match[0]));
