4952b43792ca8bc02fa891baeefa041e8650f2a6
Bryan J Swift <bryan.j.swift@gmail.com>
2012-06-23 15:52:55 -0400

    A reporter that outputs passed assertions

---
+++ src/test/php/HtmlShowPassReporter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "../lib/simpletest/reporter.php";
+
+class HtmlShowPassReporter extends HtmlReporter {
+
+  function paintPass($message) {
+    parent::paintPass($message);
+    print "<span class=\"pass\">Pass</span>: ";
+    $breadcrumb = $this->getTestList();
+    array_shift($breadcrumb);
+    print implode("->", $breadcrumb);
+    print "->$message<br />\n";
+  }
+
+}
