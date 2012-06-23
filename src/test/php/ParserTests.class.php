d8f55097beaaade8ddfba45a24ee7187258df8e2
Bryan J Swift <bryan.j.swift@gmail.com>
2012-06-23 16:14:57 -0400

    Add test suite for Parser class

--- src/test/php/ParserTests.class.php
+++ src/test/php/ParserTests.class.php
@@ -1,11 +1,53 @@
 <?php
 
 require_once "../lib/simpletest/autorun.php";
+require_once "../../main/php/Parser.class.php";
 
 class ParserTests extends UnitTestCase {
 
-  function testLoadExistingFile() {
-    $this->assertTrue(file_exists("../resources/template.html"));
+  function testParserReadsExistingNotEmptyFile() {
+    $templatePath = dirname(__FILE__) . "/../resources/template.html";
+    $this->assertTrue(file_exists($templatePath));
+    $parser = new Parser($templatePath);
+    $this->assertNotNull($parser);
+    $this->assertTrue(count($parser->tags()) > 0);
+  }
+
+  function testParserReadsExistingEmptyFile() {
+    $templatePath = dirname(__FILE__) . "/../resources/empty-template.html";
+    $this->assertTrue(file_exists($templatePath));
+    $parser = new Parser($templatePath);
+    $this->assertNotNull($parser);
+    $this->assertTrue(count($parser->tags()) === 0);
+  }
+
+  function testParserHandlesNonExistingFile() {
+    $templatePath = dirname(__FILE__) . "/../resources/nonexistant-template.html";
+    $this->assertFalse(file_exists($templatePath));
+    $parser = new Parser($templatePath);
+    $this->assertNotNull($parser);
+    $this->assertTrue(count($parser->tags()) === 0);
+    $this->assertEqual($parser->raw(), $templatePath);
+  }
+
+  function testParserReadsEmptyString() {
+    $parser = new Parser("");
+    $this->assertNotNull($parser);
+    $this->assertTrue(count($parser->tags()) === 0);
+  }
+
+  function testParserReadsNotEmptyString() {
+    $templatePath = dirname(__FILE__) . "/../resources/template.html";
+    $this->assertTrue(file_exists($templatePath));
+
+    $fh = fopen($templatePath, "r");
+    $template = fread($fh, filesize($templatePath));
+    fclose($fh);
+    $this->assertTrue(count($template) > 0);
+
+    $parser = new Parser($template);
+    $this->assertNotNull($parser);
+    $this->assertTrue(count($parser->tags()) > 0);
   }
 
 }
