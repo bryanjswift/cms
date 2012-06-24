88c3f2db9229498623553fef02ab17a110d352a8
Bryan J Swift <bryan.j.swift@gmail.com>
2012-06-24 13:28:35 -0400

    Create a test runner script in Ruby
    runner.rb uses the listen gem to watch for file changes and then execute
    the php index in src/test/php which should include all the tests

---
+++ runner.rb
@@ -0,0 +1,29 @@
+require 'rubygems'
+
+require 'listen'
+
+cont = true
+
+puts "Watching src/{main,test}/php/**/*.php for changes"
+puts "---"
+
+callback = Proc.new do |modified, added, removed|
+  puts `php src/test/php/index.php`
+  puts "---"
+end
+
+listener = Listen.to('src')
+listener = listener.filter(/\.php$/)
+listener.change(&callback)
+
+Signal.trap("SIGINT") do
+  listener.stop
+  cont = false
+  puts ""
+end
+
+listener.start(false)
+
+while cont do
+  sleep 1
+end
