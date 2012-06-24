4bcedb577637dfad0a22ea3965157765af759b86
Bryan J Swift <bryan.j.swift@gmail.com>
2012-06-24 17:30:08 -0400

    Add an easily identifiable MockTag

---
+++ src/test/php/MockTag.class.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once(dirname(__FILE__) . "/setup.php");
+
+require_once "$srcRoot/BasicTag.class.php";
+
+class MockTag extends BasicTag {
+  public function __construct($ns, $kind, $markup) {
+    parent::__construct($ns, $kind, $markup);
+  }
+
+  public function isMock() {
+    return true;
+  }
+}
